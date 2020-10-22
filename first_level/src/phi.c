76e99bd2aa6d0fe96c762c45d403f0a4a4926f61
Akli Hamitouche <akli.m.hamitouche@gmail.com>
2020-10-22 17:58:04 +0200

    0x05 done : evaluating phi with fibonacci

---
+++ first_level/src/phi.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+double phi ( void)	{
+	unsigned long long n1 = 1, n2 = 1;
+	
+	// Fibonacci(n) with n= ULL_MAX
+	for (n1 = 1, n2 = 1; (n2 + n1) > n2; n1=(n2=n1+n2) -n1);
+	printf ("%llu %llu\n", n1, n2);
+	return (double)n2/n1;
+}
+
+int main_func (void)	{
+	
+	printf ("%f\n", phi());
+	return 0;
+}
